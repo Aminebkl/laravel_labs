d6130016163230cfd609ed8bc2b070531ec19896
Aminebkl <amine.bkl@hotmail.comls>
2021-06-23 12:23:49 +0200

    controller, seeder, table about fait

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\About;
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $abouts = About::all();
+        return view("backoffice.about.all", compact("abouts"));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view("backoffice.about.create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $about = new About;
+        $about -> titre = $request -> titre;
+        $about -> description1 = $request -> description1;
+        $about -> description2 = $request -> description2;
+        $about -> bouton = $request -> bouton;
+        $about -> image = $request -> image;
+        $about -> video = $request -> video;
+
+        $about -> created_at = now();
+
+        $about -> save();
+
+        return redirect() -> route("abouts.index");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\About  $about
+     * @return \Illuminate\Http\Response
+     */
+    public function show(About $about)
+    {
+        return view("backoffice.about.show", compact("about"));
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\About  $about
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(About $about)
+    {
+        return view("backoffice.about.edit", compact("about"));
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\About  $about
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, About $about)
+    {
+        $about -> titre = $request -> titre;
+        $about -> description1 = $request -> description1;
+        $about -> description2 = $request -> description2;
+        $about -> bouton = $request -> bouton;
+        $about -> image = $request -> image;
+        $about -> video = $request -> video;
+        $about -> updated_at = now();
+
+        $about -> save();
+
+        return redirect() -> route("abouts.index");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\About  $about
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(About $about)
+    {
+        $about -> delete();
+
+        return redirect() -> back();
+    }
+}
---
+++ database/migrations/2021_06_23_095813_create_abouts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAboutsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('abouts', function (Blueprint $table) {
+            $table->string("titre");
+            $table->text("description1");
+            $table->text("description2");
+            $table->string("bouton");
+            $table->string("image");
+            $table->string("video");
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('abouts');
+    }
+}
