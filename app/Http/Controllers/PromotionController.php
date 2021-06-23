1f1cdde365a5330e5ddea57ae27f4b641bf7dc4a
Aminebkl <amine.bkl@hotmail.comls>
2021-06-23 16:42:54 +0200

    Seeder, controller, table promotion fait

---
+++ app/Http/Controllers/PromotionController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Promotion;
+use Illuminate\Http\Request;
+
+class PromotionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $promotions = Promotion::all();
+        return view("backoffice.promotion.all", compact("promotions"));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view("backoffice.promotion.create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $promotion = new Promotion;
+        $promotion -> titre = $request -> titre;
+        $promotion -> description = $request -> description;
+        $promotion -> bouton = $request -> bouton;
+        $promotion -> created_at = now();
+
+        $promotion -> save();
+
+        return redirect() -> route("promotions.index");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Promotion  $promotion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Promotion $promotion)
+    {
+        return view("backoffice.promotion.show", compact("promotion"));
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Promotion  $promotion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Promotion $promotion)
+    {
+        return view("backoffice.promotion.edit", compact("promotion"));
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Promotion  $promotion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Promotion $promotion)
+    {
+        $promotion -> titre = $request -> titre;
+        $promotion -> description = $request -> description;
+        $promotion -> bouton = $request -> bouton;
+        $promotion -> updated_at = now();
+
+        $promotion -> save();
+
+        return redirect() -> route("promotions.index");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Promotion  $promotion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Promotion $promotion)
+    {
+        $promotion -> delete();
+
+        return redirect() -> back();
+    }
+}
